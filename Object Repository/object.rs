1921d125d7a65402ffd33dda924bab981f3846e9
samuel-garcia <samuel.garcia@tokopedia.com>
2019-08-12 14:00:11 +0700

    Tsting

---
+++ Object Repository/object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>object</name>
+   <tag></tag>
+   <elementGuidId>bd3a5b0d-f610-4aee-8198-337a87fc49c5</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
